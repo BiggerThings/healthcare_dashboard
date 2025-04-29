5fb9d58929550b8baaaede08bce7fe6fb8367487
BiggerThings <nnsbiggerthings@gmail.com>
2025-04-29 05:02:49 +0200

    Added styling and UI components to Diagnosis History Component

--- src/components/DiagnosisHistory/DiagnosisHistory.tsx
+++ src/components/DiagnosisHistory/DiagnosisHistory.tsx
@@ -3,7 +3,23 @@ import "./DiagnosisHistory.css";
 export function DiagnosisHistory() {
     return (
         <div className="diagnosis-history">
-            Diagnosis History
+            <div className="diagnosis-history-header">Diagnosis History</div>
+                
+            <div className="blood-pressure-container">
+                <div className="blood-pressure-title-container">
+                    <div  className="blood-pressure-header">Blood Pressure</div>
+                        <div  className="time-period">
+                        <div className="time-period-text">Last 6 months</div>
+                        <div>
+                            <img src="/expand_more_FILL0_wght300_GRAD0_opsz24.svg" alt="arrow" />
+                        </div>
+                    </div>
+                </div>
+            
+                <div className="chart-container">
+                    d
+                </div>
+            </div>
         </div>
     )
 }
